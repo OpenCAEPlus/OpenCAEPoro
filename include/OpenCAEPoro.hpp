ebbc8f3025da91a0c4cea26960f308dc3925e008
zhangcs <zhangchensong@gmail.com>
2021-10-10 09:42:03 +0800

    Add an example for naming conventions

--- include/OpenCAEPoro.hpp
+++ include/OpenCAEPoro.hpp
@@ -1,51 +1,71 @@
-#pragma once
+/*! \file    OpenCAEPoro.hpp
+ *  \brief   Main header file for OpenCAEPoro simulator
+ *  \author  Shizhe Li
+ *  \date    Oct/01/2021
+ *
+ *-----------------------------------------------------------------------------------
+ *  Copyright (C) 2021--present by the OpenCAEPoro team. All rights reserved.
+ *  Released under the terms of the GNU Lesser General Public License 3.0 or later.
+ *-----------------------------------------------------------------------------------
+ */
+
+#ifndef __OPENCAEPORO_HEADER__
+#define __OPENCAEPORO_HEADER__
 
 // OpenCAEPoro header files
-#include "Reservoir.hpp"
+#include "Method.hpp"
 #include "OpenCAEControl.hpp"
 #include "OpenCAEOutput.hpp"
 #include "ParamRead.hpp"
-#include "Method.hpp"
+#include "Reservoir.hpp"
 #include "Timing.hxx"
 
-
-/// OpenCAEPoro is the topest structure in our simulator, in which there are Reservoir class,
-/// which stroes all the properties of reservoir, and Method class such as IMPES, and OCP_Control class
-/// which manages the params of method and time step, and OCP_output class, in which you can output the
-/// results you are interested in.
+/// Top level data structure in the OpenCAEPoro simulator
 class OpenCAEPoro
 {
 public:
-	void inputParam(ParamRead& param);
-	void setup(ParamRead& param);
-	void setupSolver();
+    /// Read input parameters to an internal structure.
+    void InputParam(ParamRead& param);
 
-	void init();
-	void run();
-	void out();
+    /// Setup reservoir based on an internal structure.
+    void SetupReservoir(ParamRead& param);
+
+    /// Setup linear solution method.
+    void SetupSolver();
+
+    /// Initialize or get intitial status of reserovir.
+    void InitReservoir();
+
+    /// Run dynamic simulation.
+    void RunSimulation();
+
+    /// Output necessary information for post-processing.
+    void OutputResults();
 
 private:
+    /// The core properties of a reservoir.
+    Reservoir reservoir;
 
-	/// the core component.
-	Reservoir			reservoir;
+    /// The IMplicit Pressure Explicit Saturation method class.
+    OCP_IMPES impes;
 
-	// Method
-	OCP_IMPES			Impes;
+    /// The Fully Implicit Method class.
+    OCP_FIM fim;
 
-	OCP_FIM				Fim;
+    /// Control class manages the params of method and time step.
+    OCP_Control control;
 
-	// control
-	OCP_Control			control;
-
-	// output file
-	OCP_Output			output;
+    /// Output class outputs the results you are interested in.
+    OCP_Output output;
 };
 
+#endif /* end if __OPENCAEPORO_HEADER__ */
 
 /*----------------------------------------------------------------------------*/
 /*  Brief Change History of This File                                         */
 /*----------------------------------------------------------------------------*/
 /*  Author              Date             Actions                              */
 /*----------------------------------------------------------------------------*/
-/*  Shizhe Li           Oct/08/2021      Create file                          */
+/*  Shizhe Li           Oct/01/2021      Create file                          */
+/*  Chensong Zhang      Oct/10/2021      Rename variables and methods         */
 /*----------------------------------------------------------------------------*/
