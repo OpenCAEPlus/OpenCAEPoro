f56b3c04f15936f79a0d303d0c3895ce71ad7d15
zhangcs <zhangchensong@gmail.com>
2021-10-27 14:39:44 +0800

    Rearrange functions in Solver class

--- include/Solver.hpp
+++ include/Solver.hpp
@@ -12,46 +12,46 @@
 #include "FluidSolver.hpp"
 #include "LinearSolver.hpp"
 
-
 #ifndef __SOLVER_HEADER__
 #define __SOLVER_HEADER__
 
 class Solver
 {
 public:
+    /// Allocate mat memory.
+    void AllocateMat(const Reservoir& rs);
+    /// Initialize the reservoir.
+    void InitReservoir(Reservoir& rs) const;
+    /// Start simulation.
+    void RunSimulation(Reservoir& rs, OCP_Control& ctrl, OCP_Output& output);
+    /// Run one time step.
+    void GoOneStep(Reservoir& rs, OCP_Control& ctrl);
 
-	/// Simulation begins
-	void RunSimulation(Reservoir& rs, OCP_Control& ctrl, OCP_Output& output);
-	/// Run one time step
-	void GoOneStep(Reservoir& rs, OCP_Control& ctrl);
-
-	/// Before solve: prepare for assembling matrix.
-	void Prepare(Reservoir& rs, OCP_DBL& dt);
-	/// Assemble and Solve: assemble mat from different parts and solve.
-	void AssembleSolve(Reservoir& rs, OCP_Control& ctrl, const OCP_DBL& dt);
-	/// Update properties after solving.
-	bool UpdateProperty(Reservoir& rs, OCP_DBL& dt);
-	/// Determine if Newton iteration is finished.
-	bool FinishNR();
-	/// Finish current timestep.
-	void FinishStep(Reservoir& rs, OCP_Control& ctrl);
-
-	/// Setup linear solver params.
-	void SetupParamLS(const string& dir, const string& file);
-	/// Allocate mat mem
-	void AllocateMat(const Reservoir& rs);
-	/// Initialize the reservoir
-	void InitReservoir(Reservoir& rs) const;
-
+    /// Before solve: prepare for assembling matrix.
+    void Prepare(Reservoir& rs, OCP_DBL& dt);
+    /// Setup linear solver params.
+    void SetupParamLS(const string& dir, const string& file);
+    /// Assemble and Solve: assemble linear system parts together then solve.
+    void AssembleSolve(Reservoir& rs, OCP_Control& ctrl, const OCP_DBL& dt);
+    /// Update properties after solving.
+    bool UpdateProperty(Reservoir& rs, OCP_DBL& dt);
+    /// Determine if Newton iteration is finished.
+    bool FinishNR();
+    /// Finish current time step.
+    void FinishStep(Reservoir& rs, OCP_Control& ctrl);
 
 private:
-
-	FluidSolver     FSolver;
-	LinearSolver	LSolver;
-
+    FluidSolver  FSolver;
+    LinearSolver LSolver;
 };
 
-
-
-
 #endif /* end if __SOLVER_HEADER__ */
+
+/*----------------------------------------------------------------------------*/
+/*  Brief Change History of This File                                         */
+/*----------------------------------------------------------------------------*/
+/*  Author              Date             Actions                              */
+/*----------------------------------------------------------------------------*/
+/*  Shizhe Li           Oct/01/2021      Create file                          */
+/*  Shizhe Li           Oct/21/2021      Change from OCPMethod to Solver      */
+/*----------------------------------------------------------------------------*/
