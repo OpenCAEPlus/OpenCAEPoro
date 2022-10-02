317d76a8aae00fd61b990de5c1b281eb22a9a614
chengxiaoxing <0001001bill@gmail.com>
2022-10-02 01:01:57 -0400

    Add test scripts

---
+++ scripts/test/linux/gnu-test.sh
@@ -0,0 +1,3 @@
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_CXX_COMPILER="g++" -DCMAKE_C_COMPILER="gcc" -DCMAKE_Fortran_COMPILER="gfortran" -DFASP_DIR="/opt/faspsolver" -DBUILD_TEST=ON -S "." -B "./build/x64-gnu-Release"
+cmake --build "./build/x64-gnu-$1" --target all
+ctest --test-dir "./build/x64-gnu-Release"
