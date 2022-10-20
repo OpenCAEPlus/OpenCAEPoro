80683eb1ba13388ce76c3b4bce5ab532552da881
chengxiaoxing <0001001bill@gmail.com>
2022-10-20 13:28:29 -0400

    using modern cmake targets for the optional packages

--- modules/OptionalFASP4BLKOIL.cmake
+++ modules/OptionalFASP4BLKOIL.cmake
@@ -7,18 +7,29 @@ option(USE_FASP4BLKOIL "Use FASP4BLKOIL" OFF)
 if(USE_FASP4BLKOIL)
 
   # set the path to find specific modules
-  set(CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/modules")
-
   set(FASP4BLKOIL_DIR "${FASP4BLKOIL_DIR}")
 
   find_package(FASP4BLKOIL)
   if(FASP4BLKOIL_FOUND)
-    add_definitions("-DWITH_FASP4BLKOIL=1")
-    include_directories(${FASP4BLKOIL_INCLUDE_DIRS})
-    set(OPTIONAL_LIBS ${OPTIONAL_LIBS} ${FASP4BLKOIL_LIBRARIES})
+    message(STATUS "INFO: FASP4BLKOIL found")
+    add_library(fasp4blkoil STATIC IMPORTED GLOBAL)
+    set_property(
+      TARGET fasp4blkoil
+      APPEND
+      PROPERTY IMPORTED_LOCATION ${FASP4BLKOIL_LIBRARIES})
+    set_property(TARGET umfpack APPEND COMPILE_DEFINITIONS
+                                "-DWITH_FASP4BLKOIL=1")
+    set_property(TARGET umfpack APPEND INTERFACE_INCLUDE_DIRECTORIES
+                                ${FASP4BLKOIL_INCLUDE_DIRS})
+    target_link_libraries(${LIBNAME} PUBLIC fasp4blkoil)
+
+    # add_definitions("-DWITH_FASP4BLKOIL=1")
+    # include_directories(${FASP4BLKOIL_INCLUDE_DIRS}) set(OPTIONAL_LIBS
+    # ${OPTIONAL_LIBS} ${FASP4BLKOIL_LIBRARIES})
   else(FASP4BLKOIL_FOUND)
     message(
-      "-- WARNING: FASP4BLKOIL was requested but not supported! Continue without it."
+      WARNING
+        "WARNING: FASP4BLKOIL was requested but not supported! Continue without it."
     )
   endif(FASP4BLKOIL_FOUND)
 
--- modules/OptionalOPENMP.cmake
+++ modules/OptionalOPENMP.cmake
@@ -1,19 +1,20 @@
-###############################################################################
-## Find compiler support for OpenMP. Since cmake 3.9, we can use 
-## find_package(OpenMP). Keep this for older cmake!!!
-###############################################################################
+# ##############################################################################
+# Find compiler support for OpenMP. Since cmake 3.9, we can use
+# find_package(OpenMP). Keep this for older cmake!!!
+# ##############################################################################
 option(USE_OPENMP "Use OPENMP" OFF)
 
 if(USE_OPENMP)
-    if(CMAKE_VERSION VERSION_GREATER 3)
-        cmake_policy(SET "CMP0054" NEW)
-    endif()
+  if(CMAKE_VERSION VERSION_GREATER 3)
+    cmake_policy(SET "CMP0054" NEW)
+  endif()
 
-    find_package(OpenMP)
-    if(OPENMP_FOUND)
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")	 
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-    else(OPENMP_FOUND)
-        message("   WARNING: OpenMP was requested but disabled!")
-    endif(OPENMP_FOUND)
+  find_package(OpenMP)
+  if(OPENMP_FOUND)
+    message(STATUS "INFO: OpenMP found")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+  else(OPENMP_FOUND)
+    message(WARNING "WARNING: OpenMP was requested but disabled!")
+  endif(OPENMP_FOUND)
 endif(USE_OPENMP)
