8274562bdfddefafaa6a1eb5c343c7f6b1d0dc14
zhangcs <zhangchensong@gmail.com>
2022-11-05 17:37:34 +0800

    Add new regression test problem

--- regression/compreg.sh
+++ regression/compreg.sh
@@ -8,24 +8,26 @@ RED='\033[0;31m'   # red color
 GREEN='\033[0;32m' # green color
 NC='\033[0m'       # no color
 
+PNUM_ALL=9
 DIRS=("spe1a"
       "spe1a"
       "spe1b"
       "spe1b" 
-      "spe9"
       "cornerpoint"
       "spe3"
-      "spe5")
+      "spe5"
+      "spe9"
+      "spe9")
 FILES=("spe1a.data" 
        "spe1a.data method=FIM dtInit=1 dtMax=100 dtMin=0.1"
        "spe1b.data"
        "spe1b.data method=FIM dtInit=1 dtMax=100 dtMin=0.1" 
-       "spe9_FIM.data"
        "CP.data    method=FIM dtInit=1 dtMax=100 dtMin=0.1"
        "spe3.data  method=FIM dtInit=1 dtMax=100 dtMin=0.1"
-       "spe5.data  method=FIM dtInit=1 dtMax=50  dtMin=0.1")
+       "spe5.data  method=FIM dtInit=1 dtMax=50  dtMin=0.1"
+       "spe9_FIM.data"
+       "spe9_IMPEC.data")
 
-PNUM_ALL=8
 if (($# > 0)) && (($1<=PNUM_ALL)); then
     PNUM=$1
 else
@@ -44,9 +46,9 @@ for ((i=0; i<PNUM; i++)); do
 
     printf "Problem $i: ${GREEN}${DIRS[i]}/${FILES[i]}${NC}\n"
     ../testOpenCAEPoro ../examples/${DIRS[i]}/${FILES[i]} > ../examples/${DIRS[i]}/log.out
-    mv    ../examples/${DIRS[i]}/$OCP_LOG  ./$i/New_IMPEC_$OCP_LOG
-    mv    ../examples/${DIRS[i]}/$OCP_SUM  ./$i/New_IMPEC_$OCP_SUM
-    mv    ../examples/${DIRS[i]}/$OCP_REV  ./$i/New_IMPEC_$OCP_REV
+    mv ../examples/${DIRS[i]}/$OCP_LOG ./$i/New_IMPEC_$OCP_LOG 2>/dev/null
+    mv ../examples/${DIRS[i]}/$OCP_SUM ./$i/New_IMPEC_$OCP_SUM 2>/dev/null
+    mv ../examples/${DIRS[i]}/$OCP_REV ./$i/New_IMPEC_$OCP_REV 2>/dev/null
     sdiff -s ./$i/Old_IMPEC_$OCP_LOG ./$i/New_IMPEC_$OCP_LOG 2>/dev/null
     sdiff -s ./$i/Old_IMPEC_$OCP_SUM ./$i/New_IMPEC_$OCP_SUM 2>/dev/null
     sdiff -s ./$i/Old_IMPEC_$OCP_REV ./$i/New_IMPEC_$OCP_REV 2>/dev/null
